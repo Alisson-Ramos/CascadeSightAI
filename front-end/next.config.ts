27ff938ee4fd1860bbd2ba132f74792554dabb4d
Alisson Santos <alisson.r.santos92@gmail.com>
2025-10-08 01:03:07 -0300

    :bookmark: build: v1.1.3 Changes chat liberado

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -4,4 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+/** next.config.js */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/cascadesightai'
+      }
+    ]
+  }
+}
+
 export default nextConfig;
